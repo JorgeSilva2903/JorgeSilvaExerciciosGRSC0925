23fda356a53fdceec48c1036af60c0b04af1e0a3
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 00:57:14 -0800

    exercicio 3

---
+++ Exercicios_Match/Exercício3.ps1
@@ -0,0 +1,19 @@
+﻿$tipoInput = Read-Host "Introduza o tipo (compra/venda)"
+$valorInput = Read-Host "Introduza o valor"
+
+
+$pedido = @{
+    "tipo"  = $tipoInput.ToLower()
+    "valor" = $valorInput
+}
+
+
+if ($pedido.tipo -eq "compra") {
+    Write-Host "Saída -> Compra de $($pedido.valor)€"
+}
+elseif ($pedido.tipo -eq "venda") {
+    Write-Host "Saída -> Venda de $($pedido.valor)€"
+}
+else {
+    Write-Host "Saída -> Pedido desconhecido"
+}
