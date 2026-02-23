48001db732e3d909686398ea149894eb7e126654
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 01:00:12 -0800

    exercicio 1

--- Exercicios_Match/Exercício1.ps1
+++ Exercicios_Match/Exercício1.ps1
@@ -1,17 +1,8 @@
-﻿$dia = Read-Host "Introduza o nome de um dia da semana"
+﻿$entrada = Read-Host "Introduza um dia da semana"
+$dia = $entrada.ToLower()
 
-$dia = $dia.ToLower()
-
-switch ($dia) {
-    { $_ -in "segunda", "terça", "quarta", "quinta", "sexta", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira" } {
-        Write-Host "Dia útil"
-    }
-    
-    { $_ -in "sábado", "sabado", "domingo" } {
-        Write-Host "Fim de semana"
-    }
-
-    Default {
-        Write-Host "Entrada inválida. Por favor, escreva um dia da semana."
-    }
+if ($dia -eq "sábado" -or $dia -eq "sabado" -or $dia -eq "domingo") {
+    Write-Host "Saída -> Fim de semana"
+} else {
+    Write-Host "Saída -> Dia útil"
 }
