3be48c9ada67d1cdac413ec50bc85ad96e0ebd59
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 01:02:09 -0800

    exercicio 2

--- Exercicios_Match/Exercício2.ps1
+++ Exercicios_Match/Exercício2.ps1
@@ -1,19 +1,8 @@
-﻿$entrada = Read-Host "Entrada"
-
+﻿$entrada = Read-Host "Introduza a classificação (Excelente, Bom, Suficiente, Insuficiente)"
 switch ($entrada.ToLower()) {
-    "excelente" {
-        Write-Host "Saída -> 90 ou mais"
-    }
-    "bom" {
-        Write-Host "Saída -> 70-89"
-    }
-    "suficiente" {
-        Write-Host "Saída -> 50-69"
-    }
-    "insuficiente" {
-        Write-Host "Saída -> Abaixo de 50"
-    }
-    Default {
-        Write-Host "Classificação não reconhecida."
-    }
+    "excelente"    { Write-Host "Saída -> 90 ou mais" }
+    "bom"          { Write-Host "Saída -> 70-89" }
+    "suficiente"   { Write-Host "Saída -> 50-69" }
+    "insuficiente" { Write-Host "Saída -> Abaixo de 50" }
+    Default        { Write-Host "Classificação não reconhecida." }
 }
