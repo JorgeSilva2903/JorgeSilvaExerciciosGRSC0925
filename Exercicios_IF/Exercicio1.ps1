3a2d5ed7e4f4f4afc80cf45a871f8f5daf0efc63
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 04:35:35 -0800

    Exercicio 1

---
+++ Exercicios_IF/Exercicio1.ps1
@@ -0,0 +1,15 @@
+﻿$totalSegundosInput = Read-Host "Introduza o valor em segundos"
+
+$total = [int]$totalSegundosInput
+
+$horas = [int]($total / 3600)
+if ($horas * 3600 -gt $total) { $horas-- }
+
+$resto = $total % 3600
+
+$minutos = [int]($resto / 60)
+if ($minutos * 60 -gt $resto) { $minutos-- }
+
+$segundosFinal = $resto % 60
+
+Write-Host "Saída esperada: $horas hora, $minutos minuto e $segundosFinal segundos."
