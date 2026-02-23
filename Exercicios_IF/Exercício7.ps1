9e053d2db89b47b7e6cddf7bc66ce26c2eca1883
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 06:53:42 -0800

    Exercício 7

---
+++ Exercicios_IF/Exercício7.ps1
@@ -0,0 +1,15 @@
+﻿$nota1 = [decimal](Read-Host "Introduza a primeira nota (Peso 2)")
+$nota2 = [decimal](Read-Host "Introduza a segunda nota (Peso 3)")
+$nota3 = [decimal](Read-Host "Introduza a terceira nota (Peso 5)")
+
+$somaPesos = 2 + 3 + 5
+$media = (($nota1 * 2) + ($nota2 * 3) + ($nota3 * 5)) / $somaPesos
+
+Write-Host "Média: $( "{0:N1}" -f $media )"
+
+if ($media -ge 6) {
+    Write-Host "Aprovado"
+}
+else {
+    Write-Host "Reprovado"
+}
