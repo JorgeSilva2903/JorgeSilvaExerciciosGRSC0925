8c90a0300072e86ad1179ec278e19b74bd6b7678
JorgeSilva2903 <jorgebsilva69@gmail.com>
2026-02-23 12:25:00 +0000

    Update Exercício10.ps1

--- Exercicios_Match/Exercício10.ps1
+++ Exercicios_Match/Exercício10.ps1
@@ -5,17 +5,17 @@ $j1 = $jogador1.ToLower()
 $j2 = $jogador2.ToLower()
 
 if ($j1 -eq $j2) {
-    Write-Host "Saída -> Empate"
+    Write-Host "Empate"
 }
 else {
     switch ("$j1 vs $j2") {
-        "pedra vs tesoura" { Write-Host "Saída -> Jogador 1 venceu" }
-        "tesoura vs papel" { Write-Host "Saída -> Jogador 1 venceu" }
-        "papel vs pedra"   { Write-Host "Saída -> Jogador 1 venceu" }
+        "pedra vs tesoura" { Write-Host "Jogador 1 venceu" }
+        "tesoura vs papel" { Write-Host "Jogador 1 venceu" }
+        "papel vs pedra"   { Write-Host "Jogador 1 venceu" }
         
-        "tesoura vs pedra" { Write-Host "Saída -> Jogador 2 venceu" }
-        "papel vs tesoura" { Write-Host "Saída -> Jogador 2 venceu" }
-        "pedra vs papel"   { Write-Host "Saída -> Jogador 2 venceu" }
+        "tesoura vs pedra" { Write-Host "Jogador 2 venceu" }
+        "papel vs tesoura" { Write-Host "Jogador 2 venceu" }
+        "pedra vs papel"   { Write-Host "Jogador 2 venceu" }
         
         Default { Write-Host "Jogada inválida detetada." }
     }
