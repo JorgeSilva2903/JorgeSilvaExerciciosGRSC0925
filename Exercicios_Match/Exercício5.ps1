b658265b41a35a6dd38cb8a95b6e5add79314e87
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 01:08:02 -0800

    exercicio 5

---
+++ Exercicios_Match/Exercício5.ps1
@@ -0,0 +1,16 @@
+﻿$entrada = Read-Host "Entrada"
+
+$msg = $entrada.ToLower()
+
+if ($msg -eq "olá" -or $msg -eq "ola" -or $msg -eq "bom dia") {
+    Write-Host "Saída -> Saudação"
+}
+elseif ($msg.EndsWith("?")) {
+    Write-Host "Saída -> Pergunta"
+}
+elseif ($msg.Contains("tchau") -or $msg.Contains("adeus")) {
+    Write-Host "Saída -> Despedida"
+}
+else {
+    Write-Host "Saída -> Mensagem genérica"
+}
