e378f6e2c4a1d78dd9b7df0c1a7aebc488d9507b
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 03:49:47 -0800

    Exercicio 7

---
+++ Exercicios_Match/Exercício7.ps1
@@ -0,0 +1,21 @@
+﻿$categoriaInput = Read-Host "Introduza a categoria (eletrónico/alimento)"
+$precoInput = Read-Host "Introduza o preço"
+
+$produto = @{
+    "categoria" = $categoriaInput.ToLower()
+    "preco"     = [decimal]$precoInput
+}
+
+if ($produto.categoria -eq "eletrónico" -or $produto.categoria -eq "eletronico") {
+    if ($produto.preco -gt 1000) {
+        Write-Host "Saída -> Produto de luxo"
+    } else {
+        Write-Host "Saída -> Produto comum"
+    }
+}
+elseif ($produto.categoria -eq "alimento") {
+    Write-Host "Saída -> Produto alimentar"
+}
+else {
+    Write-Host "Saída -> Categoria desconhecida"
+}
