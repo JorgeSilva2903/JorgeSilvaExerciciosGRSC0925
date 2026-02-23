53790a97b4bb6ed3164df8d08bfea4f783a635c0
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 06:47:11 -0800

    Exercício 5

---
+++ Exercicios_IF/Exercício5.ps1
@@ -0,0 +1,37 @@
+﻿$num1 = [int](Read-Host "Introduza o valor para num1")
+$num2 = [int](Read-Host "Introduza o valor para num2")
+$num3 = [int](Read-Host "Introduza o valor para num3")
+
+if ($num1 -le $num2 -and $num1 -le $num3) {
+    $menor = $num1
+    if ($num2 -le $num3) {
+        $meio = $num2
+        $maior = $num3
+    } else {
+        $meio = $num3
+        $maior = $num2
+    }
+}
+elseif ($num2 -le $num1 -and $num2 -le $num3) {
+    $menor = $num2
+    if ($num1 -le $num3) {
+        $meio = $num1
+        $maior = $num3
+    } else {
+        $meio = $num3
+        $maior = $num1
+    }
+}
+else {
+    $menor = $num3
+    if ($num1 -le $num2) {
+        $meio = $num1
+        $maior = $num2
+    } else {
+        $meio = $num2
+        $maior = $num1
+    }
+}
+
+Write-Host "Crescente: $menor, $meio, $maior"
+Write-Host "Decrescente: $maior, $meio, $menor"
