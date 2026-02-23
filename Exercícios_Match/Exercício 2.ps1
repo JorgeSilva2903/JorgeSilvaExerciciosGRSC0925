18c3598a127516c45df9e504d5a0f47b468fde10
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 00:41:45 -0800

    Exercício 2

---
+++ Exercícios_Match/Exercício 2.ps1
@@ -0,0 +1,19 @@
+﻿$entrada = Read-Host "Entrada"
+
+switch ($entrada.ToLower()) {
+    "excelente" {
+        Write-Host "Saída -> 90 ou mais"
+    }
+    "bom" {
+        Write-Host "Saída -> 70-89"
+    }
+    "suficiente" {
+        Write-Host "Saída -> 50-69"
+    }
+    "insuficiente" {
+        Write-Host "Saída -> Abaixo de 50"
+    }
+    Default {
+        Write-Host "Classificação não reconhecida."
+    }
+}
