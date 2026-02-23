84cfe723cf0c06f66d8e7a0a4bcdfffba5b7bcb4
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 06:51:38 -0800

    Exercício 6

---
+++ Exercicios_IF/Exercício6.ps1
@@ -0,0 +1,20 @@
+﻿$nome = Read-Host "Introduza o nome do cliente"
+$valorCompra = [decimal](Read-Host "Introduza o valor da compra (€)")
+
+if ($valorCompra -le 200) {
+    $percentagem = 0.10 
+}
+elseif ($valorCompra -le 500) {
+    $percentagem = 0.15
+}
+else {
+    $percentagem = 0.20
+}
+
+$valorDesconto = $valorCompra * $percentagem
+$totalPagar = $valorCompra - $valorDesconto
+
+Write-Host "Nome: $nome"
+Write-Host "Compra: $( "{0:N2}" -f $valorCompra )€"
+Write-Host "Desconto: $( "{0:N2}" -f $valorDesconto )€"
+Write-Host "Total a pagar: $( "{0:N2}" -f $totalPagar )€"
