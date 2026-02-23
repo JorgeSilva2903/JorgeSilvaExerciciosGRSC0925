f724154bd930d8143d7f104b7c285ea09f12b1e1
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 05:02:39 -0800

    Exercicio 3

---
+++ Exercicios_IF/Exercicio3.ps1
@@ -0,0 +1,14 @@
+﻿$num1 = [int](Read-Host "Introduza o valor para num1")
+$num2 = [int](Read-Host "Introduza o valor para num2")
+
+if ($num1 -le $num2) {
+    $menor = $num1
+    $maior = $num2
+} else {
+    $menor = $num2
+    $maior = $num1
+}
+
+Write-Host "Saída esperada:"
+Write-Host "Crescente: $menor, $maior"
+Write-Host "Decrescente: $maior, $menor"
