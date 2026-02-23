0a284de053f7bdf62853da7748f90051efb0ba2f
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-23 05:05:04 -0800

    Exercicio 4

---
+++ Exercicios_IF/Exercício4.ps1
@@ -0,0 +1,11 @@
+﻿$saldo = [decimal](Read-Host "Introduza o saldo inicial")
+$cheque = [decimal](Read-Host "Introduza o valor do cheque")
+
+if ($cheque -le $saldo) {
+    $saldoAtualizado = $saldo - $cheque
+    Write-Host "Saída esperada:"
+    Write-Host "Cheque descontado, saldo: $saldoAtualizado"
+} else {
+    Write-Host "Saída esperada:"
+    Write-Host "O cheque não pode ser descontado por falta de saldo."
+}
