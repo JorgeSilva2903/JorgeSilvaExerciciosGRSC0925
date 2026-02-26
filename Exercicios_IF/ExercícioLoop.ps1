a55ca78c4d1a25fa0b05e8e5a59a78fc4e860ba0
Jorge Silva <jorgebsilva69@gmail.com>
2026-02-26 02:43:43 -0800

    Exercícios Loop

---
+++ Exercicios_IF/ExercícioLoop.ps1
@@ -0,0 +1,15 @@
+﻿$pares = 0
+$impares = 0
+
+for ($i = 1; $i -le 10; $i++) {
+    $n = [int](Read-Host "Número $i")
+    
+    if ($n % 2 -eq 0) {
+        $pares++
+    } else {
+        $impares++
+    }
+}
+
+Write-Host "Pares: $pares"
+Write-Host "Ímpares: $impares"
