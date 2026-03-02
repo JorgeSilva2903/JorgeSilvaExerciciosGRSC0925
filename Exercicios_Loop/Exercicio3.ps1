dd77ec85e0bc76c911b6d8f09ff59cd794ca2d1f
Jorge Silva <jorgebsilva69@gmail.com>
2026-03-02 07:38:09 -0800

    Exercicio3

---
+++ Exercicios_Loop/Exercicio3.ps1
@@ -0,0 +1,12 @@
+﻿$somaNotas = 0
+
+for ($i = 1; $i -le 10; $i++) {
+    
+    $nota = Read-Host "Introduza a nota do aluno $i"
+    
+    $somaNotas = $somaNotas + $nota
+}
+
+$media = $somaNotas / 10
+
+Write-Host "A média das notas é: $media"
