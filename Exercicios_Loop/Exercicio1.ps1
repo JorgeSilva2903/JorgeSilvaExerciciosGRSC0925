e688a0c08734dab2a4ffa25ab808b1dbfcd54c8c
Jorge Silva <jorgebsilva69@gmail.com>
2026-03-02 06:52:03 -0800

    Exercicio2

--- Exercicios_Loop/Exercicio1.ps1
+++ Exercicios_Loop/Exercicio1.ps1
@@ -1,4 +1,4 @@
-﻿for ($i = 1; $i -le 30; $i++) {
+﻿for ($i = 1; $i -le 60; $i++) {
     
     if ($i % 2 -eq 0) {
         Write-Host "$i é PAR"
---
+++ Exercicios_Loop/Exercicio2.ps1
@@ -0,0 +1,11 @@
+﻿for ($i = 1; $i -le 10; $i++) {
+    
+    $numero = Read-Host "Digite o $i número"
+    
+    if ($numero % 2 -eq 0) {
+        Write-Host "$numero é par"
+    }
+    else {
+        Write-Host "$numero é ímpar"
+    }
+}
