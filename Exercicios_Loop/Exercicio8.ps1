ede72680ce4f4849b9ea9ca83a226bfcf4e4c3c3
Jorge Silva <jorgebsilva69@gmail.com>
2026-03-09 06:47:15 -0700

    Exercicios 8

---
+++ Exercicios_Loop/Exercicio8.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Começar no 10 em 10"
+for ($i = 10; $i -le 1000; $i = $i + 10) {
+    Write-Host $i
+}
+
+Write-Host "Começar no 15 em 15"
+for ($i = 15; $i -le 995; $i = $i + 10) {
+    Write-Host $i
+}
