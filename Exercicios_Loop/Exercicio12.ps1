a2838503e79194597c44e0269675e391bfdfee9d
Jorge Silva <jorgebsilva69@gmail.com>
2026-03-09 08:20:41 -0700

    Exercicios 12

---
+++ Exercicios_Loop/Exercicio12.ps1
@@ -0,0 +1,15 @@
+﻿$num = Read-Host "Introduza o número para efetuar as operações"
+$operacoesEfetuadas = 0
+
+for ($i = $num - 1; $i -ge 1; $i--) {
+    
+    Write-Host "$num + $i = " ($num + $i)
+    Write-Host "$num - $i = " ($num - $i)
+    Write-Host "$num * $i = " ($num * $i)
+    Write-Host "$num / $i = " ($num / $i)
+    Write-Host "----------------"
+
+    $operacoesEfetuadas = $operacoesEfetuadas + 4
+}
+
+Write-Host "No total, foram efetuadas $operacoesEfetuadas operações."
